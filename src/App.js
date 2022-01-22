edeb99880d66815fb25f205b387bf398ca5b72a3
Berlin <50ta2012@gmail.com>
2022-01-22 20:39:39 +0800

    pagination init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table } from 'react-bootstrap';
+import { Pagination } from 'react-bootstrap'
+
+function makeTable(value, index){
+  return (
+    <tr key={index}>
+      <td>{value.id}</td>
+      <td>{value.name}</td>
+      <td>{value.age}</td>
+    </tr>
+  );
+}
+
+function usersGererator(size){
+  let items = [];
+  for (let i = 0; i < size; i++) {
+    items.push({ id: i + 1, name: `Name ${i + 1}`, age: 18 + i });
+  }
+  return items;
+}
+
+const users = usersGererator(10);
+
+function makePagination(size){
+  let items = []
+  for(let i = 1; i <= size; i++){
+    items.push(
+      <Pagination.Item key={i} onClick={this.props.active = true}>
+      {i}
+    </Pagination.Item>,
+    );
+  }
+
+  return items;
+}
+
+let pageOnClick;
+
+function pageOnClickHandler(page){
+	pageOnClick = page;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Bootstrap Table</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(makeTable)}
+        </tbody>
+      </Table>
+      <Pagination>
+        {makePagination(10)}
+      </Pagination>
     </div>
   );
 }
---
+++ src/PaginationPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PaginationPage extends React.Component{
+    handleClick(page){
+        alert(page);
+    }
+
+    render(){
+        return(
+            <div>123</div>
+        );
+    }
+}
+
+export default PaginationPage;
